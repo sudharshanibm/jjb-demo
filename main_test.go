d082fc21d8fe2f4207f030259654b20429961d64
Sudharshan Muralidharan <50818126+sudharshan3@users.noreply.github.com>
2024-01-17 19:09:03 +0530

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -30,15 +30,6 @@ func TestGetAllCourses(t *testing.T) {
 	}
 }
 
-// Additional test functions for other API endpoints can be added similarly
-
-func resetCourses() {
-	courses = []Courses{
-		{CourseID: "1329dfs", CourseName: "React JS", Price: 38.98, Author: &Author{Name: "Sudharshan", Website: "http://github.com/sudharshan3"}},
-		{CourseID: "sdaff4321", CourseName: "GO Lang", Price: 156.98, Author: &Author{Name: "Sudharshan", Website: "http://github.com/sudharshan3"}},
-	}
-}
-
 func TestGetOneCourse(t *testing.T) {
 	resetCourses()
 
@@ -56,39 +47,11 @@ func TestGetOneCourse(t *testing.T) {
 		t.Errorf("Handler returned wrong status code: got %v want %v", status, http.StatusOK)
 	}
 
-	var expected Courses
-	for _, c := range courses {
-		if c.CourseID == "1329dfs" {
-			expected = c
-			break
-		}
-	}
-
-	var actual Courses
-	err = json.NewDecoder(rr.Body).Decode(&actual)
+	var course Courses
+	err = json.Unmarshal(rr.Body.Bytes(), &course)
 	if err != nil {
-		t.Fatal(err)
+		t.Errorf("Error unmarshalling JSON: %v", err)
 	}
 
-	if !coursesEqual(actual, expected) {
-		t.Errorf("Handler returned unexpected body:\n got  %v\n want %v", actual, expected)
-	}
+	// Now you can perform assertions on the 'course' object
 }
-
-// Additional test functions for other API endpoints can be added similarly
-
-func coursesEqual(c1, c2 Courses) bool {
-	return c1.CourseID == c2.CourseID &&
-		c1.CourseName == c2.CourseName &&
-		c1.Price == c2.Price &&
-		authorEqual(c1.Author, c2.Author)
-}
-
-func authorEqual(a1, a2 *Author) bool {
-	if a1 == nil || a2 == nil {
-		return a1 == a2
-	}
-	return a1.Name == a2.Name && a1.Website == a2.Website
-}
-
-// Add similar functions for testing other endpoints like addOneCourse, updateCourse, deleteCourse, etc.
