e32ffe667dbeccfcd75bf20688118a87f6012dde
Sudharshan Muralidharan <50818126+sudharshan3@users.noreply.github.com>
2024-02-05 12:43:19 +0530

    integration-successfull

--- run.sh
+++ run.sh
@@ -5,8 +5,8 @@ echo "${username}:${password}"
 C:/Users/SudharshanMuralidhar/AppData/Local/Programs/Python/Python39/python.exe -m venv .
 source ./Scripts/activate      
 pip install jenkins-job-builder
-jenkins-jobs --conf ./jenkins_jobs.ini update  ./matrix-pipeline.yaml --delete-old
+jenkins-jobs --conf ./jenkins_jobs.ini update ./matrix-pipeline.yaml --delete-old
 curl -X POST -u ${username}:${password} \
-    http://localhost:8080/job/shan-matrix-1/build
+    http://localhost:8080/job/multi-config-job-1/buildWithParameters?PARAMETER=Successful
 
 
